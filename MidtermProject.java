d4db98b93cbf00ca20806bae13e3615f414231a0
Ice8tack <113066494+Ice8tack@users.noreply.github.com>
2022-10-05 18:17:25 -0500

    Made the class picking system

--- MidtermProject.java
+++ MidtermProject.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
 public class MidtermProject
 {
+
     public static void main(String[] args)
     {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Hello and welcome to our game!" + "Please enter your name");
+        String name = input.nextLine();
+
+        System.out.println("Pick a class from among the ones availble: " + "\nWizard (W), Swordsman (S), Rogue (R), or Bard (R)");
+        char pickClass = input.nextcharacter();
+
+        if( pickClass == W)
+        {
+            int playerMaxHealth = 10;
+            int playerMaxMana = 25;
+            int playerAttack = 5;
+        }
+        else if (pickClass == S)
+        {
+            int playerMaxHealth = 10;
+            int playerMaxMana = 25;
+            int playerAttack = 5;
+        }
+        else if (pickClass == R)
+        {
+            int playerMaxHealth = 10;
+            int playerMaxMana = 25;
+            int playerAttack = 5;
+        }
+        else if (pickClass == B)
+        {
+            int playerMaxHealth = 10;
+            int playerMaxMana = 25;
+            int playerAttack = 5;
+        }
+
     }
 }
