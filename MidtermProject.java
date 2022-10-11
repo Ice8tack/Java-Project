ff947223f25f38edafcc3380e36f99e4ddb423fe
Andrinor1 <andrinor2001@gmail.com>
2022-10-10 19:12:51 -0500

    Monster class added

--- MidtermProject.java
+++ MidtermProject.java
@@ -1,40 +1,29 @@
 import java.util.Scanner;
 public class MidtermProject
 {
+    public static boolean isClassValid(char a){
+        if (a == 'W' || a == 'S' || a == 'R' || a == 'B'){
+            return true;
+        }
+        return false;
+    }
+    
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Hello and welcome to our game!" + "Please enter your name");
+        System.out.println("Hello and welcome to our game! Please enter your name");
         String name = input.nextLine();
 
-        System.out.println("Pick a class from among the ones available: " + "\nWizard (W), Swordsman (S), Rogue (R), or Bard (R)");
+        System.out.println("Pick a class from among the ones available: " + "\nWizard (W), Swordsman (S), Rogue (R), or Bard (B)");
         char pickClass = input.next().charAt(0);
-
-        if( pickClass == 'W')
-        {
-            int playerMaxHealth = 10;
-            int playerMaxMana = 25;
-            int playerAttack = 5;
+        while (!isClassValid(pickClass)){
+            System.out.println("Invalid class selection. Please select an available class.");
+            pickClass = input.next().charAt(0);
         }
-        else if (pickClass == 'S')
-        {
-            int playerMaxHealth = 10;
-            int playerMaxMana = 25;
-            int playerAttack = 5;
-        }
-        else if (pickClass == 'R')
-        {
-            int playerMaxHealth = 10;
-            int playerMaxMana = 25;
-            int playerAttack = 5;
-        }
-        else if (pickClass == 'B')
-        {
-            int playerMaxHealth = 10;
-            int playerMaxMana = 25;
-            int playerAttack = 5;
-        }
-
+        Player user = new Player(pickClass);
+        //Monster goblin = new Monster(10,3);
+        
+        input.close();
     }
 }
---
+++ Monster.java
@@ -0,0 +1,10 @@
+public class Monster{
+    int maxHealth;
+    int currentHealth;
+    int attackDamage;
+    
+    public Monster(int health, int damage){
+        maxHealth = health;
+        attackDamage = damage;
+    }
+}
